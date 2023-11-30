4381780e15ecf63be0f32bb5f1e82deac4463452
Jayden Skuladottir <sask5403@colorado.edu>
2023-11-29 22:45:57 -0700

    Update Pokemon.jsx

--- Pokemon/src/Pokemon.jsx
+++ Pokemon/src/Pokemon.jsx
@@ -6,6 +6,11 @@ import React, { useEffect, useState } from 'react';
 
 export default function Pokemon() {
   const [thePokemons, setPokemons] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    setIsShown(true);
+  };
   
   // CHAT GPT used here for debugging
   useEffect(() => {
@@ -39,40 +44,36 @@ export default function Pokemon() {
 
   return (
 
-        <div className='flex-parent'>
+      <div className='flex-parent'>
         {thePokemons.map((pokemon, index) => (
           <div className='card'key={index}>
-          
-          {/* Names */}
-            <h1> {pokemon.name} </h1>
+            {/* Names */}
+              <h1> {pokemon.name} </h1>
 
-           {/* Images */}
-            <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
-            
+            {/* Images */}
+              <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
+              
+            <button onClick = {handleClick}>More Info</button>
+            {/* Show elements on click */}
+            {isShown && (
+                <ul>
+                  {/* Pokemon Types */}
+                    <li>Type: {pokemon.types[0].type.name} </li>
+                  {/* Height & Weight */}
+                    <li>Height: {pokemon.height}</li>
+                    <li>Weight: {pokemon.weight}</li>
+                  {/* Regular Stats */}
+                    <li>Regular Stat 1: {pokemon.stats[0].stat.name} </li>
+                    <li>Regular Stat 2: {pokemon.stats[1].stat.name} </li>
+                    <li>Regular Stat 3: {pokemon.stats[2].stat.name} </li>
+                </ul>
+            )}
           </div>
         ))}
       </div>
        
   );
 }
-
-
-
-
-// <ul>
-// {/* Pokemon Types */}
-// <li>Type: {pokemon.types[0].type.name} </li>
-// </ul>
-
-
-// {/* Height & Weight */}
-// <ul>
-// <li>Height: {pokemon.height}</li>
-// </ul>
-
-// <ul>
-// <li>Weight: {pokemon.weight}</li>
-// </ul>
 
 // {/* Regular Stats */}
 // <ul>
