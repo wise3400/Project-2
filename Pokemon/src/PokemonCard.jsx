f63e1a040f71750b11b1e611f8c885667c82af44
Wilson Seet <70073191+wise3400@users.noreply.github.com>
2023-11-30 18:11:30 -0700

    11/30

--- Pokemon/src/PokemonCard.jsx
+++ Pokemon/src/PokemonCard.jsx
@@ -18,31 +18,34 @@ export default function Pokemon({pokemon, index}) {
 
               {/* Images */}
                 <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
-                
-              <button onClick = {handleClick}>More Info</button>
+              
+              <button className='more-button' onClick = {handleClick}>More Info</button>
+
               {/* Show elements on click */}
               {isShown && (
-                  <ul>
+                 <div className='full-card'>
+                   <ul>
                     {/* Pokemon Types */}
-                      <li>Type: {pokemon.types[0].type.name} </li>
+                      <li><b>Type:</b> {pokemon.types[0].type.name} </li>
                     {/* Height & Weight */}
-                      <li>Height: {pokemon.height}</li>
-                      <li>Weight: {pokemon.weight}</li>
+                      <li><b>Height:</b> {pokemon.height}</li>
+                      <li><b>Weight:</b> {pokemon.weight}</li>
                     {/* Regular Stats */}
-                      <li>Regular Stat 1: {pokemon.stats[0].stat.name} </li>
-                      <li>Regular Stat 2: {pokemon.stats[1].stat.name} </li>
-                      <li>Regular Stat 3: {pokemon.stats[2].stat.name} </li>
-                      <li>Regular Stat 4: {pokemon.stats[3].stat.name} </li>
-                      <li>Regular Stat 5: {pokemon.stats[4].stat.name} </li>
-                      <li>Regular Stat 6: {pokemon.stats[5].stat.name} </li>
+                      <li><b>Regular Stat 1:</b> {pokemon.stats[0].stat.name} </li>
+                      <li><b>Regular Stat 2:</b> {pokemon.stats[1].stat.name} </li>
+                      <li><b>Regular Stat 3:</b> {pokemon.stats[2].stat.name} </li>
+                      <li><b>Regular Stat 4:</b> {pokemon.stats[3].stat.name} </li>
+                      <li><b>Regular Stat 5:</b> {pokemon.stats[4].stat.name} </li>
+                      <li><b>Regular Stat 6:</b> {pokemon.stats[5].stat.name} </li>
                     {/* Base Stats */}
-                      <li>Base Stat 1a: {pokemon.stats[0].base_stat} </li>
-                      <li>Base Stat 1b: {pokemon.stats[1].base_stat} </li>
-                      <li>Base Stat 1c: {pokemon.stats[2].base_stat} </li>
-                      <li>Base Stat 1d: {pokemon.stats[3].base_stat} </li>
-                      <li>Base Stat 1e: {pokemon.stats[4].base_stat} </li>
-                      <li>Base Stat 1f: {pokemon.stats[5].base_stat} </li>
+                      <li><b>Base Stat 1a:</b> {pokemon.stats[0].base_stat} </li>
+                      <li><b>Base Stat 1b:</b> {pokemon.stats[1].base_stat} </li>
+                      <li><b>Base Stat 1c:</b> {pokemon.stats[2].base_stat} </li>
+                      <li><b>Base Stat 1d:</b> {pokemon.stats[3].base_stat} </li>
+                      <li><b>Base Stat 1e:</b> {pokemon.stats[4].base_stat} </li>
+                      <li><b>Base Stat 1f:</b> {pokemon.stats[5].base_stat} </li>
                   </ul>
+                 </div>
               )}
             </div>
   )
