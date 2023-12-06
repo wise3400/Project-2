761617d16e65ea440417af61d6f86737886b4c56
ana <anabellemahadevan@gmail.com>
2023-12-05 17:23:12 -0700

    push

--- Pokemon/src/PokemonCard.jsx
+++ Pokemon/src/PokemonCard.jsx
@@ -46,16 +46,18 @@ export default function Pokemon({pokemon, index}) {
                         </div>
                       </div>
 
+                      <li> <b>{pokemon.stats[1].stat.name}:</b> {pokemon.stats[1].base_stat}</li>
+
 {/*        
 
-                      <li><b>Regular Stat 2:</b> {pokemon.stats[1].stat.name} </li>
-                      <li><b>Regular Stat 3:</b> {pokemon.stats[2].stat.name} </li>
+                      
+                      
                       <li><b>Regular Stat 4:</b> {pokemon.stats[3].stat.name} </li>
                       <li><b>Regular Stat 5:</b> {pokemon.stats[4].stat.name} </li>
                       <li><b>Regular Stat 6:</b> {pokemon.stats[5].stat.name} </li>
 
 
-                      <li><b>Base Stat 1b:</b> {pokemon.stats[1].base_stat} </li>
+                      
                       <li><b>Base Stat 1c:</b> {pokemon.stats[2].base_stat} </li>
                       <li><b>Base Stat 1d:</b> {pokemon.stats[3].base_stat} </li>
                       <li><b>Base Stat 1e:</b> {pokemon.stats[4].base_stat} </li>
