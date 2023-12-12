8381d934547b3e34e3574349d96e76563dc15448
ana <anabellemahadevan@gmail.com>
2023-12-12 15:32:36 -0700

    filter :)

--- Pokemon/src/PokemonList.jsx
+++ Pokemon/src/PokemonList.jsx
@@ -1,6 +1,6 @@
 
 import PokemonCard from './PokemonCard.jsx'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
 // https://www.codingthesmartway.com/how-to-fetch-api-data-with-react/
 // https://www.geeksforgeeks.org/react-js-pokemon-app/
@@ -9,6 +9,8 @@ import React, { useEffect, useState } from 'react';
 export default function Pokemon() {
   const [thePokemons, setPokemons] = useState([]);
   const [isInfoShown, setIsShown] = useState(false);
+  const [selectedType, setSelectedType] = useState("all")
+  const filteredPokemon = useMemo(() => filterPokemon(thePokemons, selectedType), [thePokemons, selectedType])
 
   useEffect(() => {
     const getPokemon = async () => 
@@ -39,13 +41,49 @@ export default function Pokemon() {
     getPokemon();
   }, []);
 
+  function filterPokemon(pokemons, type) {
+    if (type !== "all") {
+      return pokemons.filter(pokemon => pokemon.types[0].type.name === type)
+    }
+    else {
+      return pokemons
+    }
+  }
+
   return (
 
-      <div className='flex-parent'>
-        {thePokemons.map((pokemon, index) => (
-          <PokemonCard pokemon = {pokemon} index = {index} key = {index}  showInfo={isInfoShown} setShowInfo={setIsShown}/>
+      <div>
+        <select 
+          value={selectedType}
+          onChange={(e) => setSelectedType(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="normal">Normal</option>
+          <option value="fire">Fire</option>
+          <option value="water">Water</option>
+          <option value="grass">Grass</option>
+          <option value="electric">Electric</option>
+          <option value="ice">Ice</option>
+          <option value="fighting">Fighting</option>
+          <option value="poison">Poison</option>
+          <option value="ground">Ground</option>
+          <option value="flying">Flying</option>
+          <option value="psychic">Psychic</option>
+          <option value="bug">Bug</option>
+          <option value="rock">Rock</option>
+          <option value="ghost">Ghost</option>
+          <option value="dragon">Dragon</option>
+          <option value="dark">Dark</option>
+          <option value="steel">Steel</option>
+          <option value="fairy">Fairy</option>
+        </select>
+        <button onClick={() => console.log(thePokemons)}></button>
+        <div className='flex-parent'>
+          {filteredPokemon.map((pokemon, index) => (
+            <PokemonCard pokemon = {pokemon} index = {index} key = {index}  showInfo={isInfoShown} setShowInfo={setIsShown}/>
 
-        ))}
+          ))}
+        </div>
       </div>
        
   );
